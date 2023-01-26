57bf3fd5853c0e071422234852ce21ab4fa6ead7
Dương Doãn <tranduongdoan0922@gmail.com>
2023-01-26 21:24:43 +0700

    auto warning when missing file

--- layout-web/index.js
+++ layout-web/index.js
@@ -1,3 +1,16 @@
+function checkFileExists(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("HEAD", url, false);
+  xhr.send();
+
+  if (xhr.status == "404") {
+    console.warn("File not found: " + url);
+    return false;
+  } else {
+    return true;
+  }
+}
+
 $(document).ready(function () {
   console.log("layout-v1.js loaded");
   //your code here
@@ -7,8 +20,15 @@ $(document).ready(function () {
       var html = "./components/" + $(this).data("include") + ".html";
       var css = "./styles/" + $(this).data("include") + ".css";
       var js = "./js/" + $(this).data("include") + ".js";
-      $("head").append(`<link rel="stylesheet" href="${css}" />`);
+
+      const errors = [html, css, js].map((item) => {
+        return checkFileExists(item);
+      });
+      const isValid = errors.every((item) => item === true);
+      if (!isValid) return;
+
       $(this).load(html, () => {
+        $("head").append(`<link rel="stylesheet" href="${css}" />`);
         $.getScript(js);
       });
 
